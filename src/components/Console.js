85814ee71cf4121009fb0d57b2d91443cd28dd2e
calebcox725 <caleb.cox@flatironschool.com>
2017-07-18 18:54:01 -0400

    Add scrollToBottom() feature to console

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,66 +1,75 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Console = () => {
-  const Wrapper = styled.div`
-    height: 71px;
-    margin: 4px;
-    overflow-y: scroll;
-    box-sizing: content-box;
+class Console extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [
+        'Propane and propane accessories. Dang it Peggy. Buaaaaaaah. I tell you what. Dang it Bobby. America, hooray, gosh darn it. Lorem ipsum I tell you what. I love Texas. Lol. Jabroni.',
+        'Propane and propane accessories. Dang it Peggy. Buaaaaaaah. I tell you what. Dang it Bobby. America, hooray, gosh darn it. Lorem ipsum I tell you what. I love Texas. Lol. Jabroni.',
+        'Propane and propane accessories. Dang it Peggy. Buaaaaaaah. I tell you what. Dang it Bobby. America, hooray, gosh darn it. Lorem ipsum I tell you what. I love Texas. Lol. Jabroni.',
+        'Propane and propane accessories. Dang it Peggy. Buaaaaaaah. I tell you what. Dang it Bobby. America, hooray, gosh darn it. Lorem ipsum I tell you what. I love Texas. Lol. Jabroni.',
+      ],
+    };
+  }
 
-    &::-webkit-scrollbar {
-      background-color: black;
-      width: 8px;
-    }
+  componentDidMount() {
+    this.scrollToBottom();
+  }
 
-    &::-webkit-scrollbar-thumb {
-      background-color: white;
-    }
-  `;
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
 
-  const Message = styled.p`
-    color: white;
-    margin: 4px 0;
-    padding-left: 12px;
-    font-family: Monaco, Consolas, "Lucida Console", monospace;
-    font-size: 10px;
-    position: relative;
+  scrollToBottom() {
+    this.node.scrollTop = this.node.scrollHeight;
+  }
 
-    &::before {
-      content: '>';
-      position: absolute;
-      left: 0;
-    }
-  `;
+  render() {
+    const Wrapper = styled.div`
+      height: 71px;
+      margin: 4px;
+      overflow-y: scroll;
+      box-sizing: content-box;
 
-  return (
-    <Wrapper>
-      <Message>
-        Propane and propane accessories. Dang it Peggy.
-        Buaaaaaaah. I tell you what. Dang it Bobby.
-        America, hooray, gosh darn it. Lorem ipsum I
-        tell you what. I love Texas. Lol. Jabroni.
+      &::-webkit-scrollbar {
+        background-color: black;
+        width: 8px;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: white;
+      }
+    `;
+
+    const Message = styled.p`
+      color: white;
+      margin: 4px 0;
+      padding-left: 12px;
+      font-family: Monaco, Consolas, "Lucida Console", monospace;
+      font-size: 10px;
+      position: relative;
+
+      &::before {
+        content: '>';
+        position: absolute;
+        left: 0;
+      }
+    `;
+
+    const messages = this.state.messages.map(message => (
+      <Message key={Math.random()}>
+        {message}
       </Message>
-      <Message>
-        Propane and propane accessories. Dang it Peggy.
-        Buaaaaaaah. I tell you what. Dang it Bobby.
-        America, hooray, gosh darn it. Lorem ipsum I
-        tell you what. I love Texas. Lol. Jabroni.
-      </Message>
-      <Message>
-        Propane and propane accessories. Dang it Peggy.
-        Buaaaaaaah. I tell you what. Dang it Bobby.
-        America, hooray, gosh darn it. Lorem ipsum I
-        tell you what. I love Texas. Lol. Jabroni.
-      </Message>
-      <Message>
-        Propane and propane accessories. Dang it Peggy.
-        Buaaaaaaah. I tell you what. Dang it Bobby.
-        America, hooray, gosh darn it. Lorem ipsum I
-        tell you what. I love Texas. Lol. Jabroni.
-      </Message>
-    </Wrapper>
-  );
-};
+    ));
+
+    return (
+      <Wrapper innerRef={(node) => { this.node = node; }}>
+        {messages}
+      </Wrapper>
+    );
+  }
+}
 
 export default Console;
