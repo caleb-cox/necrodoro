6085bbd96396ffc0633160e3443fdf06e649c4b5
calebcox725 <caleb.cox@flatironschool.com>
2017-07-28 04:54:41 -0400

    Popup button style tweaks

--- src/components/PopupButton.js
+++ src/components/PopupButton.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 
 const PopupButtonWrapper = styled.button`
   width: 100%;
-  margin: 2px;
+  margin: 4px;
   padding: 8px;
   border: 1px solid white;
   font-family: 'Nova Mono', monospace;
+  line-height: 1em;
   text-transform: uppercase;
   outline: none;
 
